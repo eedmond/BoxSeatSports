0ef19d2aa8854f4650c1e0c748f56ee9ae0bb947
Eric Edmond <Eric Edmond>
2016-11-11 22:19:08 -0800

    Add support for link 2 and an obscure link I found

--- js/CleanUpMainPage.js
+++ js/CleanUpMainPage.js
@@ -18,7 +18,16 @@ $(document).ready(function()
 	AddClassToElementFamily($("img[alt='FirstRowSports']"));
 	AddClassToElementFamily($("link[href*='match.css'"));
 	
+	// Get rid of everything we don't approve of
 	$("*").not(".PartOfCleanView").remove();
+	
+	// These are typically the "Watch in HD" links that are total bull crap
+	$("a[href*='downloadplayer'").remove();
+	
+	// Get rid of all popup behavior, this doesn't work
+	// perfectly, needs to be improved
+	$("a").removeAttr("click");
 	$("a").removeAttr("onclick");
+	$("a").removeAttr("onmouseenter");
 });
 $("script").remove();
--- js/CleanUpVideoPlayer.js
+++ js/CleanUpVideoPlayer.js
@@ -13,17 +13,17 @@ function RemoveAllButElementIfFound(element)
 	$("*").not(".PartOfCleanView").remove();
 }
 
-function CleanupIFirstrowsportsFrame()
+function CleanUpIFirstrowsportsFrame()
 {
 	RemoveAllButElementIfFound($("#player"));
 }
 
-function CleanupIFirstrowEmbedFrame()
+function CleanUpIFirstrowEmbedFrame()
 {
 	RemoveAllButElementIfFound($("iframe[src*='04stream.com/ebb.php'"));
 }
 
-function Cleanup04StreamFrame()
+function CleanUp04StreamFrame()
 {
 	var videoPlayer = $("#div1");
 	
@@ -38,6 +38,50 @@ function Cleanup04StreamFrame()
 	});
 	
 	RemoveAllButElementIfFound(videoPlayer);
+	
+	// Remove all click events in this frame because they usually create popups that adblock doesn't' catch
+	var allElements = $("*");
+	allElements.remoteAttr("click");
+	allElements.removeAttr("onclick");
+	
+	if (videoPlayer.find("*").length === 0)
+	{
+		videoPlayer.bind("DOMSubtreeModified", function()
+		{
+			videoPlayer.parent().find(".helpText").remove();
+		});
+		videoPlayer.parent().append('<h1 class="helpText" style="color:white;">Looks like you need to enable Flash. Look for the puzzle piece in the top right and select "Run all plugins this time"</h1>');
+	}
+}
+
+function CleanUpWizSportsFrame()
+{
+	RemoveAllButElementIfFound($("#frame"));
+}
+
+function CleanUpBroadcastFrame()
+{
+	RemoveAllButElementIfFound($("#player"));
+}
+
+function CleanUpLive9Frame()
+{
+	RemoveAllButElementIfFound($("#player"));
+	RemoveAllButElementIfFound($("iframe[src*='sawlive.tv/embed/watch'"));
+}
+
+function JwPlayerStarted()
+{
+	RemoveAllButElementIfFound($("#jwplayer1"));
+}
+
+function CleanUpSawLiveFrame()
+{
+	// Wait for video to start, then we're free to remove everyone else
+	$("#jwplayer1").bind("DOMSubtreeModified", JwPlayerStarted);
+	
+	// Manually fire this fellow's click event to start the video player
+	$("#imgclose").trigger("click");
 }
 
 function FrameHandler(urlPortion, CleanUpFrame)
@@ -46,11 +90,25 @@ function FrameHandler(urlPortion, CleanUpFrame)
 	this.CleanUpFrame = CleanUpFrame;
 }
 
-var ifirstrowMainPage = new FrameHandler("ifirstrowus.eu/watch", CleanupIFirstrowsportsFrame);
-var ifirstrowEmbed = new FrameHandler("firstrowas.co/embed", CleanupIFirstrowEmbedFrame);
-var _04StreamFrame = new FrameHandler("04stream.com/ebb.php", Cleanup04StreamFrame);
+// Link 1
+var ifirstrowMainPage = new FrameHandler("ifirstrowus.eu/watch", CleanUpIFirstrowsportsFrame);
+var ifirstrowEmbed = new FrameHandler("firstrowas.co/embed", CleanUpIFirstrowEmbedFrame);
+var _04StreamFrame = new FrameHandler("04stream.com/ebb.php", CleanUp04StreamFrame);
 
-var frameHandlers = {ifirstrowMainPage, ifirstrowEmbed, _04StreamFrame};
+// Link 2
+var live9Frame = new FrameHandler("live9.net", CleanUpLive9Frame);
+var sawliveFrame = new FrameHandler("sawlive.tv/embed/watch", CleanUpSawLiveFrame);
+
+// Obscure links
+var wizhdFrame = new FrameHandler("wizhdsports.be", CleanUpWizSportsFrame);
+var broadcastFrame = new FrameHandler("bro.adca.st/stream.php", CleanUpBroadcastFrame);
+
+var frameHandlers =
+{
+	ifirstrowMainPage, ifirstrowEmbed, _04StreamFrame,
+	live9Frame, sawliveFrame,
+	wizhdFrame, broadcastFrame
+};
 
 function UrlContainsSubstring(substring)
 {
